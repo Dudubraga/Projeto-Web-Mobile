05daa8c5899f491c9cba2c95f87645546af892ef
belamedeirosbl <belamedeirosbl@gmail.com>
2025-04-07 18:10:01 -0300

    configurando banco de dados do Back4App no projeto e passando o id para número.

--- projeto-quiz/app/periodo/[id]/page.js
+++ projeto-quiz/app/periodo/[id]/page.js
@@ -1,20 +1,35 @@
-'use client';
-import { useParams } from 'next/navigation';
-import { Cabecalho } from '@/components/cabecalho';
+"use client";
+import { useParams } from "next/navigation";
+import { Cabecalho } from "@/components/cabecalho";
+import { useEffect, useState } from "react";
+import Parse from "@/lib/parseConfig";
 
 export default function PeriodoPage() {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    return (
-        <>
-        <Cabecalho />
-        <div className='box-container'>
-        <div className='home-container'>
+  let periodoNumero; ///Passar o periodo para o numero, facilitando a manipulação no bdd
+  if (id === "primeiro-periodo") {
+    periodoNumero = 1;
+  } else if (id === "segundo-periodo") {
+    periodoNumero = 2;
+  } else if (id === "terceiro-periodo") {
+    periodoNumero = 3;
+  } else if (id === "quarto-periodo") {
+    periodoNumero = 4;
+  } else if (id === "quinto-periodo") {
+    periodoNumero = 5;
+  } else {
+    periodoNumero = "ID inválido";
+  }
 
-            <h1>Quiz do {id.replace('-', ' ')}</h1>
-            {/* Aqui você pode puxar as perguntas do banco usando o ID */}
-            </div>
+  return (
+    <>
+      <Cabecalho />
+      <div className="box-container">
+        <div className="home-container">
+          <h1>Quiz do {id.replace("-", " ")}</h1>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
---
+++ projeto-quiz/lib/parseConfig.js
@@ -0,0 +1,9 @@
+import Parse from "parse";
+
+Parse.initialize(
+  "VIXpaiT1kchv9ArEyiX7nanbliyP9yw5xJmHxwhl",
+  "AwzEUN6vIQqPCqxvk2DV1Je0eUBpu671YIoTZPh3"
+);
+Parse.serverURL = "https://parseapi.back4app.com/";
+
+export default Parse;
