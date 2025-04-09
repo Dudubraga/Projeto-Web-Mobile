8f2ca7fc011e2f771df7e7b22d5bbb1456fc004c
belamedeirosbl <belamedeirosbl@gmail.com>
2025-04-08 22:39:55 -0300

    criando condicional para não permitir usuários não logados terem acesso ao quiz

--- projeto-quiz/app/page.js
+++ projeto-quiz/app/page.js
@@ -1,8 +1,33 @@
 "use client";
 import Link from "next/link";
 import { Cabecalho } from "@/components/cabecalho";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Parse from "@/lib/parseConfig";
 
 export default function HomePage() {
+  const router = useRouter();
+  const [usuarioLogado, setUsuarioLogado] = useState(null);
+  const [mensagem, setMensagem] = useState(""); // mensagem de aviso
+
+  //ve se o usuário ta logado quando a página carrega
+  useEffect(() => {
+    const user = Parse.User.current();
+    setUsuarioLogado(user);
+  }, []);
+
+  //checa se ta logado antes de mandar para o quiz
+  const acessarQuiz = (periodo) => {
+    if (usuarioLogado) {
+      router.push(`/periodo/${periodo}`);
+    } else {
+      setMensagem("loga ai primeiro");
+      setTimeout(() => {
+        router.push("/login");
+      }, 2000); // espera 2 segundos pra mandar pro login
+    }
+  };
+
   return (
     <>
       <Cabecalho />
@@ -11,12 +36,25 @@ export default function HomePage() {
           <h1>Quiz</h1>
           <p>Escolha seu periodo:</p>
           <div className="period-links">
-            <Link href="/periodo/primeiro-periodo">Primeiro Período</Link>
-            <Link href="/periodo/segundo-periodo">Segundo Período</Link>
-            <Link href="/periodo/terceiro-periodo">Terceiro Período</Link>
-            <Link href="/periodo/quarto-periodo">Quarto Período</Link>
-            <Link href="/periodo/quinto-periodo">Quinto Período</Link>
+            <button onClick={() => acessarQuiz("primeiro-periodo")}>
+              Primeiro Período
+            </button>
+            <button onClick={() => acessarQuiz("segundo-periodo")}>
+              Segundo Período
+            </button>
+            <button onClick={() => acessarQuiz("terceiro-periodo")}>
+              Terceiro Período
+            </button>
+            <button onClick={() => acessarQuiz("quarto-periodo")}>
+              Quarto Período
+            </button>
+            <button onClick={() => acessarQuiz("quinto-periodo")}>
+              Quinto Período
+            </button>
           </div>
+          {mensagem && (
+            <p style={{ color: "red", marginTop: "10px" }}>{mensagem}</p>
+          )}
         </div>
       </div>
     </>
